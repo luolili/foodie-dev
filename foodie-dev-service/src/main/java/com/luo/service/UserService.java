6bfe569c78abe614523a26f4eb8980f00cd7f5d8
Administrator <2084267015@qq.com>
2019-11-27 21:23:39 +0800

    mapper

--- foodie-dev-service/src/main/java/com/luo/service/UserService.java
+++ foodie-dev-service/src/main/java/com/luo/service/UserService.java
@@ -1,8 +1,11 @@
 package com.luo.service;
 
 import com.luo.pojo.Stu;
+import com.luo.pojo.Users;
+import com.luo.pojo.bo.UserBO;
 
 public interface UserService {
     boolean queryUsernameIsExist(String username);
 
+    Users createUser(UserBO userBO);
 }
--- foodie-dev-service/src/main/java/com/luo/service/impl/UserServiceImpl.java
+++ foodie-dev-service/src/main/java/com/luo/service/impl/UserServiceImpl.java
@@ -1,8 +1,12 @@
 package com.luo.service.impl;
 
+import com.luo.enums.Sex;
 import com.luo.mapper.UsersMapper;
 import com.luo.pojo.Users;
+import com.luo.pojo.bo.UserBO;
 import com.luo.service.UserService;
+import com.luo.utils.DateUtil;
+import com.luo.utils.MD5Utils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
@@ -22,5 +26,24 @@ public class UserServiceImpl implements UserService {
         Users result = usersMapper.selectOneByExample(userExample);
 
         return !(result==null);
+    }
+
+    @Override
+    @Transactional
+    public Users createUser(UserBO userBO) {
+        Users user = new Users();
+        user.setUsername(userBO.getUsername());
+        try {
+            user.setPassword(MD5Utils.getMD5Str(userBO.getPassword()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        //默认
+        user.setNickname(userBO.getUsername());
+        user.setFace("");
+        user.setBirthday(DateUtil.stringToDate("1900-01-01"));
+user.setSex(Sex.secret.type);
+        return user;
     }
 }
